071e87d9c5d4a72ca41ce1b4b3ca643e6c0c4021
Nebojsa Brankovic <nbrankovic@gmail.com>
2020-05-13 09:59:10 +0200

    change pod spec

--- HyperIOSToolbox/Reachability/NetworkListenerController.swift
+++ HyperIOSToolbox/Reachability/NetworkListenerController.swift
@@ -31,15 +31,4 @@ open class NetworkListenerController: UIViewController, NetworkStatusListener {
         super.viewWillDisappear(animated)
         ConnectionManager.shared.removeListener(listener: self)
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
